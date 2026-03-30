6bf60b607d60cd6cc5a838b03708901a11d8ca6e
victorgsouza6-prog <victor.g.souza6@aluno.senai.br>
2026-03-30 10:48:33 -0300

    Update minhaaula.js

--- 07 - importacao/minhaaula.js
+++ 07 - importacao/minhaaula.js
@@ -11,3 +11,11 @@ import { imprimir } from "./funcoes-auxiliares.js";
 const numeros = [10, 5, 8, 2, 9, 15, 3];
 
 imprimir(numeros);
+
+import { numerosPares } from './funcoes-aux.js'
+
+const numeros = [1, 3, 9, 10, 12]
+
+const resultado = numerosPares(numeros)
+
+console.log(`Números pares: ${resultado}`)
